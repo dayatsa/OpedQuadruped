f6f67a3f43e3c994f205521a24fee59c4c5a0f73
Dayat Saputra <dayatsaputra87@gmail.com>
2021-10-28 12:29:03 +0700

    imu

--- oped/oped_teleopp/scripts/oped/testOped.py
+++ oped/oped_teleopp/scripts/oped/testOped.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python
-from Imu import *
 
-imu = Imu()
-
-# license removed for brevity
+from __future__ import print_function
 import rospy
 from std_msgs.msg import String
 
+from Imu import *
+
+
+imu = Imu()
+
 def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
+    # pub = rospy.Publisher('chatter', String, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
+        pitch, roll = imu.getPitchRoll()
+        print("pitch: {}, roll: {}".format(pitch, roll))
+
+        # hello_str = "hello world %s" % rospy.get_time()
+        # rospy.loginfo(hello_str)
+        # pub.publish(hello_str)
         rate.sleep()
 
 if __name__ == '__main__':
